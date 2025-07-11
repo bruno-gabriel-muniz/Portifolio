f726f76d27338d93a4044e2569242dd60befeb87
bruno-gabriel-muniz <brunomunizlourenco@gmail.com>
2025-07-11 12:52:44 -0300

    refactor: script.js adicionado

---
+++ script.js
@@ -0,0 +1,30 @@
+//Função que foca em um elemento específico da página.
+function focusOn(elementId){
+    const elemento = document.getElementById(elementId);
+    const y = (
+        elemento.getBoundingClientRect().top + 
+        window.scrollY - 200
+    );
+    window.scrollTo({ top: y, behavior: "smooth" });
+}
+//Abre o Linkedin
+function openLinkedin(){
+    window.open(
+        'https://www.linkedin.com/in/bruno-gabriel-ss',
+        '_blank'
+    )
+}
+//Abre o GitHub
+function openGitHub(){
+    window.open(
+        'https://github.com/bruno-gabriel-muniz',
+        '_blank'
+    )
+}
+//Abre o meu curriculo
+function openCurriculo(){
+    window.open(
+        'https://drive.google.com/file/d/10KkzjztvmBAJS6GJTPs86sSRFsgW0d31/view?usp=sharing',
+        '_blank'
+    )
+}
